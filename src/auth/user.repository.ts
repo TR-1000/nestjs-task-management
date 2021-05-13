62da519a7ef047b7a1dd6921fa77f5bdded084b1
TR-1000 <t.l.ross@outlook.com>
2021-05-13 16:15:50 -0500

    sign-up funtionality added

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,9 +1,30 @@
 import { EntityRepository, Repository } from "typeorm";
+import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
 import { User } from "./user.entity";
-
+import * as bcrypt from 'bcrypt';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
-    
+
+    async signUp(authCredentialsDto: AuthCredentialsDto) {
+        const { username, password }  = authCredentialsDto;
+        const user = new User();
+        user.username = username;
+        user.salt = await bcrypt.genSalt();
+        user.password = await this.hashPassword(password, user.salt);
+
+
+       try {
+        await user.save();
+       } catch (error) {
+           console.log(error.detail);
+           return error;
+       }
+
+    }
+
+    private async hashPassword(password: string, salt: string): Promise<string> {
+        return bcrypt.hash(password, salt);
+    }
 
 }
